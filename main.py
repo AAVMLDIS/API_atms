1bbd5464da8512b5e2a6e70bab4dffb06a87065e
AAVMLDIS <149337470+AAVMLDIS@users.noreply.github.com>
2024-01-16 00:16:10 -0800

    main.py

--- main.py
+++ main.py
@@ -150,11 +150,8 @@ def predict_class_by_adress(adress: str):
     return 'Вы промахнулись мимо сетки городов, попробуйте снова'
 
 @app.post("/predict_items")
-def predict_items(file: UploadFile = File()) -> FileResponse:
-    df = pd.DataFrame({'Город': 1})
-    df.to_csv('result.csv')
-    response = FileResponse(path='result.csv', media_type='text/csv')
-    return response
+def predict_items(file: UploadFile = File()):
+    return 123
 
 # @app.post("/file/upload-file")
 # def upload_file(file: UploadFile=File()):
